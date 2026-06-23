a0a3042d914e659587e0e5647c421f3b08888efb
zhangfugui1994 <1326220417@qq.com>
2026-06-23 16:41:48 +0800

    update: 从管理面板推送

--- script.js
+++ script.js
@@ -1,16 +1,97 @@
 // 商品数据（由 parse_posts.py 自动生成，直接嵌入）
 const productsData = [
-  {"title": "顶级网黄 困困狗 情人节专辑流出 淫乱骚母狗被肌肉猛男无套猛操 第一视角沉浸式性爱日记！【全55.42G】", "subtitle": "颜值:9.5 | 身材:9.7 | 稀缺度:SSR", "size": "55.42G", "cover_image": "https://tuchuang-aac.pages.dev/api/cfile/AgACAgUAAyEGAATrbgq5AAICNmo41Y-x6USKBW_Sloj7DUpP9Y3xAALOEmsbIGPIVTE_gK2YkwFeAQADAgADeQADPAQ", "cover_local": "", "share_link": "https://share.note.sx/l0reagd7#xtkfug6wcdib/pbWwCRawaNaNAgnwumZobjhxirdfp8", "name": "困困狗", "updated": "2026-05-19T17:48:04+08:00"},
-  {"title": "推特符文战士 九条南无 Cos初音未来 粉嫩酮体搭配纹身野辣诱惑 阳具爆插白虎蜜穴美乳颤抖不停【全22.5G】", "subtitle": "颜值:9.6 | 身材:9.8 | 稀缺度:SSR", "size": "22.5G", "cover_image": "https://tuchuang-aac.pages.dev/api/cfile/AgACAgUAAyEGAATrbgq5AAICP2o41gPl1MszCbAoLzOAF7ekdESAAALYEmsbIGPIVSRHszaEwZ8DAQADAgADdwADPAQ", "cover_local": "QQ20260211-084016.png", "share_link": "https://share.note.sx/2lwbbxjr#LTybsR0tXwfgmzvwPW3OU6LvJaqg1llnQVkXPm+89SA", "name": "九条南无", "updated": "2026-05-19T17:50:54+08:00"},
-  {"title": "极品福利姬 Anaimiya奶咪 3月最新福利 骚气热舞扭臀甩胯诱惑无限 丰乳肥臀全景展现风骚无比【全202.88G】", "subtitle": "颜值:9.8 | 身材:9.8 | 稀缺度:SSR", "size": "202.88G", "cover_image": "https://tuchuang-aac.pages.dev/api/cfile/AgACAgUAAyEGAATrbgq5AAICN2o41fBdJ5wQq1aBIfn6X02PUyi_AALPEmsbIGPIVQsj6FN9K1eHAQADAgADeQADPAQ", "cover_local": "", "share_link": "https://share.note.sx/zfl9ezgi#Q4EPK0vZMstKWZsDUOWSdqssp610IbXCDrcESfyr+Z0", "name": "Anaimiya奶咪", "updated": "2026-05-19T17:48:22+08:00"},
-  {"title": "极品长腿黑丝女神 AsiaLynn 户外公厕制服露出诱惑 道具自慰高潮连连！", "subtitle": "颜值:9.8 | 身材:9.8 | 稀缺度:SSR", "size": "273.56G", "cover_image": "https://tuchuang-aac.pages.dev/api/cfile/AgACAgUAAyEGAATrbgq5AAICOGo41fMFPHp-ZvbiAT_2Jv3jTTG9AALQEmsbIGPIVUd7_4DuoPZxAQADAgADdwADPAQ", "cover_local": "", "share_link": "https://share.note.sx/fy32xwdp#OIB9hNb3hujv4JRbipc9BQ08vjchFpJEvqA4THbQYZU", "name": "Asia-Lynn", "updated": "2026-05-19T17:48:42+08:00"},
-  {"title": "台湾知名御姐 辛尤里 自慰私拍大作 摄影师手持道具疯狂挑逗白虎嫩穴 高潮喷水双眼迷离享受酥麻快感【全118.82G】", "subtitle": "颜值:9.8 | 身材:9.8 | 稀缺度:SSR", "size": "118.82G", "cover_image": "https://tuchuang-aac.pages.dev/api/cfile/AgACAgUAAyEGAATrbgq5AAICOWo41fUwdX9rOO03xkwRGJub4KAyAALSEmsbIGPIVQABi8t9IKs9vQEAAwIAA3cAAzwE", "cover_local": "6ww2p1ni2d9aby5gu5pp1firb0q7zews.jpg", "share_link": "https://share.note.sx/5luzxi9y#jE5Thr6ZEgVTHXCtItxSeXeKGEepk3JUVXU+O9NahDs", "name": "辛尤里", "updated": "2026-03-23T13:56:31+08:00"},
-  {"title": "推特网黄 汐梦瑶 大尺度私拍 骚浪小妖精按摩床下偷偷跪舔深喉偷吃肉棒 口爆满嘴浓精一脸满足！【全100.5G】", "subtitle": "颜值:9.8 | 身材:9.8 | 稀缺度:SSR", "size": "100.5G", "cover_image": "https://tuchuang-aac.pages.dev/api/cfile/AgACAgUAAyEGAATrbgq5AAICOmo41ffn_Dr9GPNQTT2mmV5V8kljAALTEmsbIGPIVbcX1CUKn0XdAQADAgADeQADPAQ", "cover_local": "", "share_link": "https://share.note.sx/9242runb#EaIQ+z3HElARIUC8U0MiqCezkck9Y+djBwJgykcHUJ0", "name": "汐梦瑶", "updated": "2026-05-19T17:49:18+08:00"},
-  {"title": "饼干姐姐 淫秽新作 调教绿奴老公 丝袜塞逼封精 口交足交凌辱肉棒 多体位交欢化身精液仓库！【全567.30G】", "subtitle": "颜值:9.8 | 身材:9.8 | 稀缺度:SSR", "size": "567.30G", "cover_image": "https://tuchuang-aac.pages.dev/api/cfile/AgACAgUAAyEGAATrbgq5AAICO2o41fq5G8Z3QiYkFseMTn1ENUXIAALUEmsbIGPIVU8XYmHDOfPdAQADAgADeQADPAQ", "cover_local": "1u9b92mqt2xs040vfs4xrbefbs46ey39.jpeg", "share_link": "https://share.note.sx/yjgek3fx#MEdi1x1xtS6d7rMdsr5MlKZjXxf4EmvH9OoHVhUzciY", "name": "饼干姐姐", "updated": "2026-05-19T17:49:37+08:00"},
-  {"title": "高人气微胖网黄 pupu噗噗 抖音主播被助手拍视频威胁 化身母狗胯下卖力侍奉阳具！【全106G】", "subtitle": "颜值:9.4 | 身材:9.6 | 稀缺度:SSR", "size": "106G", "cover_image": "https://tuchuang-aac.pages.dev/api/cfile/AgACAgUAAyEGAATrbgq5AAICPGo41fxnNeTuYvlTD_Xw7nKqczBzAALVEmsbIGPIVeL4jJyCT3G4AQADAgADeAADPAQ", "cover_local": "", "share_link": "https://share.note.sx/fr3diy13#CgO1J7EMusQwjKJjD6qLmVeUfuGyEvXn0b6GsUwHnRo", "name": "噗噗", "updated": "2026-05-19T17:49:58+08:00"},
-  {"title": "推特网黄 台北娜娜 新作反差婊姐2花絮流出 肉丝腿交口交寸止捆绑狂玩 灵活舌尖狠钻嫩穴 爽到失控！【全784.64G】", "subtitle": "颜值:9.3 | 身材:9.5 | 稀缺度:SSR", "size": "784.64G", "cover_image": "https://tuchuang-aac.pages.dev/api/cfile/AgACAgUAAyEGAATrbgq5AAICPWo41f6wxAJa2286uHr42ChCGKxwAALWEmsbIGPIVbtRK4LkMj2bAQADAgADeAADPAQ", "cover_local": "", "share_link": "https://share.note.sx/agnw01mb#rLKkfWTm7DrEtaXhE2RDH2e8MF0Y0r0o4jP4wwiC+qI", "name": "台北娜娜", "updated": "2026-05-19T17:50:28+08:00"},
-  {"title": "巨乳尤物 御梦子 COS尼尔哈2B性欲挑战 阳具猛插爽出阿黑颜 粉嫩酮体暴力蹂躏 高潮浪叫BUFF叠满！【全37.3G】", "subtitle": "颜值:9.5 | 身材:9.7 | 稀缺度:SSR", "size": "37.3G", "cover_image": "https://tuchuang-aac.pages.dev/api/cfile/AgACAgUAAyEGAATrbgq5AAICPmo41gABTssOk-rLyETqYiT5opd4rwAC1xJrGyBjyFWVvExaAsKRHAEAAwIAA3kAAzwE", "cover_local": "图床/09.御梦子/ia_100000102.jpg", "share_link": "https://share.note.sx/hc2h2848#s9V8SK2UW/rRZ+T1oUvbmg6nTwsibntYofFukbpru2o", "name": "御梦子", "updated": "2026-05-19T17:50:43+08:00"}
+  {
+    "title": "顶级网黄 困困狗 情人节专辑流出 淫乱骚母狗被肌肉猛男无套猛撞 第一视角沉浸式性爱日记！【全55.42G】",
+    "subtitle": "颜值:9.5 | 身材:9.7 | 稀缺度:SSR",
+    "size": "55.42G",
+    "cover_image": "https://tuchuang-aac.pages.dev/api/cfile/AgACAgUAAyEGAATrbgq5AAICNmo41Y-x6USKBW_Sloj7DUpP9Y3xAALOEmsbIGPIVTE_gK2YkwFeAQADAgADeQADPAQ",
+    "share_link": "https://share.note.sx/l0reagd7#xtkfug6wcdib/pbWwCRawaNaNAgnwumZobjhxirdfp8",
+    "name": "困囵狗",
+    "updated": "2026-05-19T17:48:04+08:00"
+  },
+  {
+    "title": "哈特朗文禁战士 九条南无 Cos初音未来 粉嫩冥体搭配肝野辛酸性感 阳具爆插白蚂蜜洞美乳抖斗不止【全22.5G】",
+    "subtitle": "颜值:9.6 | 身材:9.8 | 稀缺度:SSR",
+    "size": "22.5G",
+    "cover_image": "https://tuchuang-aac.pages.dev/api/cfile/AgACAgUAAyEGAATrbgq5AAICP2o41gPl1MszCbAoLzOAF7ekdESAAALYEmsbIGPIVSRHszaEwZ8DAQADAgADdwADPAQ",
+    "share_link": "https://share.note.sx/2lwbbxjr#LTybsR0tXwfgmzvwPW3OU6LvJaqg1llnQVkXPm+89SA",
+    "name": "九条南无",
+    "updated": "2026-05-19T17:50:54+08:00"
+  },
+  {
+    "title": "极品福利妹 Anaimiya奶咪 3月最新福利 骚气热舞扭屁疩嶛诱惑无限 丰乳肥皋全景展现风骚无比【全202.88G】",
+    "subtitle": "颜值:9.8 | 身材:9.8 | 稀缺度:SSR",
+    "size": "202.88G",
+    "cover_image": "https://tuchuang-aac.pages.dev/api/cfile/AgACAgUAAyEGAATrbgq5AAICN2o41fBdJ5wQq1aBIfn6X02PUyi_AALPEmsbIGPIVQsj6FN9K1eHAQADAgADeQADPAQ",
+    "share_link": "https://share.note.sx/zfl9ezgi#Q4EPK0vZMstKWZsDUOWSdqssp610IbXCDrcESfyr+Z0",
+    "name": "Anaimiya奶咪",
+    "updated": "2026-05-19T17:48:22+08:00"
+  },
+  {
+    "title": "极品长腿黑丝女神 AsiaLynn 户外公厨制服露出诱惑 道具自感高潮连连！",
+    "subtitle": "颜值:9.8 | 身材:9.8 | 稀缺度:SSR",
+    "size": "273.56G",
+    "cover_image": "https://tuchuang-aac.pages.dev/api/cfile/AgACAgUAAyEGAATrbgq5AAICOGo41fMFPHp-ZvbiAT_2Jv3jTTG9AALQEmsbIGPIVUd7_4DuoPZxAQADAgADdwADPAQ",
+    "share_link": "https://share.note.sx/fy32xwdp#OIB9hNb3hujv4JRbipc9BQ08vjchFpJEvqA4THbQYZU",
+    "name": "Asia-Lynn",
+    "updated": "2026-05-19T17:48:42+08:00"
+  },
+  {
+    "title": "台湾知名御姐 辛尤里 自感私拍大作 摄影师手持道具疯狂挑喵白蚂嫩穴 高潮喷水双眼迷迪享受酺麻快感【全118.82G】",
+    "subtitle": "颜值:9.8 | 身材:9.8 | 稀缺度:SSR",
+    "size": "118.82G",
+    "cover_image": "https://tuchuang-aac.pages.dev/api/cfile/AgACAgUAAyEGAATrbgq5AAICOWo41fUwdX9rOO03xkwRGJub4KAyAALSEmsbIGPIVQABi8t9IKs9vQEAAwIAA3cAAzwE",
+    "share_link": "https://share.note.sx/5luzxi9y#jE5Thr6ZEgVTHXCtItxSeXeKGEepk3JUVXU+O9NahDs",
+    "name": "辛尤里",
+    "updated": "2026-03-23T13:56:31+08:00"
+  },
+  {
+    "title": "哈特网黄 汐梦瑶 大尺度私拍 骚浪小妖精按摩床下偷偷跪喵深咸肉棒 口爆满嘴浓精一脸满足！【全100.5G】",
+    "subtitle": "颜值:9.8 | 身材:9.8 | 稀缺度:SSR",
+    "size": "100.5G",
+    "cover_image": "https://tuchuang-aac.pages.dev/api/cfile/AgACAgUAAyEGAATrbgq5AAICOmo41ffn_Dr9GPNQTT2mmV5V8kljAALTEmsbIGPIVbcX1CUKn0XdAQADAgADeQADPAQ",
+    "share_link": "https://share.note.sx/9242runb#EaIQ+z3HElARIUC8U0MiqCezkck9Y+djBwJgykcHUJ0",
+    "name": "汐梦瑶",
+    "updated": "2026-05-19T17:49:18+08:00"
+  },
+  {
+    "title": "饼干姐姐 淫羞新作 调教绿厨老公 丝袜塞逿精口交足交侥凉冻肉棒 多体位交欢化身精液仓库！【全567.30G】",
+    "subtitle": "颜值:9.8 | 身材:9.8 | 稀缺度:SSR",
+    "size": "567.30G",
+    "cover_image": "https://tuchuang-aac.pages.dev/api/cfile/AgACAgUAAyEGAATrbgq5AAICO2o41fq5G8Z3QiYkFseMTn1ENUXIAALUEmsbIGPIVU8XYmHDOfPdAQADAgADeQADPAQ",
+    "share_link": "https://share.note.sx/yjgek3fx#MEdi1x1xtS6d7rMdsr5MlKZjXxf4EmvH9OoHVhUzciY",
+    "name": "饼干姐姐",
+    "updated": "2026-05-19T17:49:37+08:00"
+  },
+  {
+    "title": "高人气微肉网黄 pupu嗒嗒 哈音主播被助手拍视频威胁 化身母狗跈下卖力侍奉阳具！【全106G】",
+    "subtitle": "颜值:9.4 | 身材:9.6 | 稀缺度:SSR",
+    "size": "106G",
+    "cover_image": "https://tuchuang-aac.pages.dev/api/cfile/AgACAgUAAyEGAATrbgq5AAICPGo41fxnNeTuYvlTD_Xw7nKqczBzAALVEmsbIGPIVeL4jJyCT3G4AQADAgADeAADPAQ",
+    "share_link": "https://share.note.sx/fr3diy13#CgO1J7EMusQwjKJjD6qLmVeUfuGyEvXn0b6GsUwHnRo",
+    "name": "嗒嗒",
+    "updated": "2026-05-19T17:49:58+08:00"
+  },
+  {
+    "title": "哈特网黄 台北卜卜 新作反差婀姐2花料流出 肉丝腿口交寸止缓挟狂玩 灵活舌尖待钻嫩穴 爽到失控！【全784.64G】",
+    "subtitle": "颜值:9.3 | 身材:9.5 | 稀缺度:SSR",
+    "size": "784.64G",
+    "cover_image": "https://tuchuang-aac.pages.dev/api/cfile/AgACAgUAAyEGAATrbgq5AAICPWo41f6wxAJa2286uHr42ChCGKxwAALWEmsbIGPIVbtRK4LkMj2bAQADAgADeAADPAQ",
+    "share_link": "https://share.note.sx/agnw01mb#rLKkfWTm7DrEtaXhE2RDH2e8MF0Y0r0o4jP4wwiC+qI",
+    "name": "台北卜卜",
+    "updated": "2026-05-19T17:50:28+08:00"
+  },
+  {
+    "title": "巨乳嫇物 御梦子 COS尼尔哈2B性欲挑战 阳具猜插爽出阿哈颜 粉嫩冥体暴力雲沩 高潮浪叫BUFF堆满！【全37.3G】",
+    "subtitle": "颜值:9.5 | 身材:9.7 | 稀缺度:SSR",
+    "size": "37.3G",
+    "cover_image": "https://tuchuang-aac.pages.dev/api/cfile/AgACAgUAAyEGAATrbgq5AAICPmo41gABTssOk-rLyETqYiT5opd4rwAC1xJrGyBjyFWVvExaAsKRHAEAAwIAA3kAAzwE",
+    "share_link": "https://share.note.sx/hc2h2848#s9V8SK2UW/rRZ+T1oUvbmg6nTwsibntYofFukbpru2o",
+    "name": "御梦子",
+    "updated": "2026-05-19T17:50:43+08:00"
+  }
 ];
+
 let posts = JSON.parse(JSON.stringify(productsData));
 let slideIndex = 0;
 let slideTimer;
